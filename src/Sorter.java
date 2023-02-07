c49a96b23ab254b2bb50f2a18b0d7bddbb905421
7ussainLZ <hussainlutfi@hotmail.com>
2023-02-07 16:56:58 +0300

    add insrtion sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -22,5 +22,21 @@ public class Sorter {
         }
         return array;
     }
-    
+    public void insertionsort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int r = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (r >= 0 && arr[r] > key) {
+                arr[r + 1] = arr[r];
+                r = r - 1;
+            }
+            arr[r + 1] = key;
+        }
+    }
 }
