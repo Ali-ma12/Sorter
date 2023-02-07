f2061ac9731d0e77aef5ef545c53fdd86e9be51c
Ali M <ali@Alis-MacBook-Air.local>
2023-02-07 16:24:13 +0300

    Added the selection sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,5 +2,25 @@ public class Sorter {
     public static void main(String[] args) {
         
     }
+
+    public int[] selectionSort(int[] array){
+        for(int i = 0; i < array.length - 1; i ++){
+            int currentMin = array[i];
+            int currentMinIndex = i;
+
+            for(int j = i + 1; j < array.length; j ++){
+                if(array[j] < currentMin){
+                    currentMin = array[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if(currentMinIndex != i){
+                array[currentMinIndex] = array[i];
+                array[i] = currentMin;
+            }
+        }
+        return array;
+    }
     
 }
